fafdd4395ad2c083b508e234501d38382be635f3
SeaZeeZee <86711383+SeaZeeZee@users.noreply.github.com>
2021-07-27 13:02:31 +0200

    Adding gusset to index

--- shift/src/index.js
+++ shift/src/index.js
@@ -3,11 +3,12 @@ import plugins from '@freesewing/plugin-bundle'
 import config from '../config'
 import draftBody from './body'
 import draftSleeve from './sleeve'
-
+import draftGusset from './gusset'
 // Create new design
 const Pattern = new freesewing.Design(config, plugins)
 
 // Attach the draft methods to the prototype
 Pattern.prototype.draftBody = draftBody
 Pattern.prototype.draftSleeve  = draftSleeve
+Pattern.prototype.draftGusset = draftGusset
 export default Pattern
