92c51f630bc1baf164f525428e6bf013f8bf6883
SeaZeeZee <86711383+SeaZeeZee@users.noreply.github.com>
2021-07-28 13:48:09 +0200

    sleeve points and path

--- shift/src/sleeve.js
+++ shift/src/sleeve.js
@@ -20,6 +20,17 @@ let sleeveWidth = measurements.biceps + (100 * options.bicepsEase)
 
 let sleeveLength = measurements.shoulderToElbow * options.sleeveBonus
 
+points.sleeveLeft = new Point(0, 0)
+points.sleeveRight = new Point(0, sleeveWidth)
+points.sleeveBottomLeft = new Point(sleeveLength, 0)
+points.sleeveBottomRight = new Point(sleeveLength, sleeveWidth)
+
+paths.sleeve = new Path()
+.move(points.sleeveRight)
+.line(points.sleeveLeft)
+.line(points.sleeveBottomLeft)
+.line(points.sleeveBottomRight)
+.close();
 
 
 
