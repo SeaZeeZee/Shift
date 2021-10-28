0267d799fa124dce26b585b70df47f5cd0d21a89
SeaZeeZee <86711383+SeaZeeZee@users.noreply.github.com>
2021-10-28 11:24:46 +0200

    Sleeve

--- shift/example/src/pattern/src/sleeve.js
+++ shift/example/src/pattern/src/sleeve.js
@@ -16,6 +16,23 @@ export default function (part) {
     } = part.shorthand()
 //design pattern here
 
+let sleeveWidth = measurements.biceps + (100 * options.bicepsEase)
+
+let sleeveLength = measurements.shoulderToElbow * options.sleeveBonus
+
+points.sleeveLeft = new Point(0, 0)
+points.sleeveRight = new Point(0, sleeveWidth)
+points.sleeveBottomLeft = new Point(sleeveLength, 0)
+points.sleeveBottomRight = new Point(sleeveLength, sleeveWidth)
+
+paths.sleeve = new Path()
+  .move(points.sleeveRight)
+  .line(points.sleeveLeft)
+  .line(points.sleeveBottomLeft)
+  .line(points.sleeveBottomRight)
+  .close();
+
+
 
       // Complete?
   if (complete) {
